86d7decf19ceed314d226eb80ff784031b88093b
carter3689 <carter3689@gmail.com>
2020-01-07 15:16:31 -0600

    Second Day Flask Lecture with Forms and submitting to the server using Flask Forms

--- decemberblog/routes.py
+++ decemberblog/routes.py
@@ -1,7 +1,20 @@
 from decemberblog import app
 from flask import render_template
+from decemberblog.forms import SignupForm
 
 #Home Route
 @app.route("/")
 def home():
     return render_template("home.html")
+
+# Sign Up Route
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+    signupForm = SignupForm()
+    
+    username = signupForm.username.data
+    email = signupForm.email.data
+    password = signupForm.password.data
+    print(username,email,password)
+
+    return render_template("signup.html",signupform = signupForm)
